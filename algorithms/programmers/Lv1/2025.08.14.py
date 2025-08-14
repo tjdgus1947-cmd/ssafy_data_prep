7551a20e5f111610c15ab4ecd9cf287d9ff57911
tjdgus01 <tjdgus1947@gmail.com>
2025-08-14 14:49:36 +0900

    Update 2025.08.14.py

--- algorithms/programmers/Lv1/2025.08.14.py
+++ algorithms/programmers/Lv1/2025.08.14.py
@@ -4,7 +4,6 @@
 angle1 = int(input())
 angle2 = int(input())
 sum_angle = (angle1 + angle2)%360
-
 print(sum_angle)
 
 # 수 나누기
@@ -13,7 +12,6 @@ answer = 0
 for _ in range(int(len(str(number))/2)):
     answer += number % 100
     number //= 100
-
 print(answer)
 
 # 병과 분류
