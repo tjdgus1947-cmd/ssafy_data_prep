49d86733d2a5d98e5d5867d5e73de675a6799af8
tjdgus01 <tjdgus1947@gmail.com>
2025-08-14 13:12:01 +0900

    Create 2025.08.14.py

---
+++ algorithms/baekjoon/bronze/2025.08.14.py
@@ -0,0 +1,31 @@
+- PB_1000 A+B
+A, B = map(int, input().split())
+print(A+B)
+  
+- PB_1002 터렛  
+N = int(input())
+
+for _ in range(N):
+  x1, y1, r1, x2, y2, r2 = map(int, input().split())
+  dx = x1-x2
+  dy = y1-y2
+  d2 = dx*dx + dy*dy
+  rp = r1+r2
+  rm = abs(r1-r2)
+  rp2 = rp*rp
+  rm2 = rm*rm
+
+  if d2 == 0:
+    if r1 == r2:
+      print(-1)
+    else : print(0)
+
+  else:
+    if (d2>rp2) or (d2<rm2): 
+      print(0)
+    elif (d2==rp2) or (d2==rm2):
+      print(1)
+    else : print(2)
+     
+
+- PB_1004 어린 왕자
